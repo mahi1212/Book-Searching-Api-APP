d7f9b243d4d2092fdd81eb8e6f9b38dd75fa830b
mahi1212 <mahi121.mr@gmail.com>
2021-09-02 15:32:29 +0600

    Maximum done without image alternative

--- app.js
+++ app.js
@@ -9,31 +9,45 @@ const searchResult = searchText => {
     url = `https://openlibrary.org/search.json?q=${searchText}`
     fetch(url)
     .then(res => res.json())
-    .then(data => showDetails(data))
-    // .then(data => console.log(data))
+    .then(data => loadDetails(data.docs))
 }
 
-const showDetails = details =>{
-    // console.log(details)
-    const resultBox = document.getElementById('result-box')
-    const div = document.createElement('div')
-    div.innerHTML = `
-    <h3>Total Search Result : ${details.numFound}</h3>
+const loadDetails = results => {
+    console.log(results)
+    const resultBox = document.getElementById('result-box') 
+    const countDivShow = document.getElementById('countDivShow') 
+    const parentDiv = document.createElement('div')
+    // Counting search result
+    const countResult = document.createElement('div')
+    countResult.innerHTML = `
+    <h3>Total Search Result : ${results.length}</h3>
     `
-    div.classList.add('text-ceter')
-    resultBox.append(div)
-    loadDetails(details.docs)
-}
+    countDivShow.append(countResult)
+    // For Each Using
+    results.forEach(result => {
+        const div = document.createElement('div')
+        const imageUrl = (`https://covers.openlibrary.org/b/id/${result.cover_i}-M.jpg`)
+        div.innerHTML = `
+            <img src = ${imageUrl ? imageUrl:'No Image'}  width='200px' height='300px'>
+            <h5> Name : ${result.title}</h5>
+            <h5>Author Name: ${result.author_name}</h5>
+            <h5>Publish year : ${result.first_publish_year}</h5>
+            <h6>Publishers : ${result.publisher}</h6>
+        `
+        // Addig styles to child div
+        div.classList.add('p-1')
+        div.style.border = '3px solid green'
+        div.style.borderRadius = '10px'
+        div.style.color = '#000'
+        div.style.padding = '10px'
+        div.style.margin = '3px'
 
-// const loadDetails = results => {
-//     console.log(results)
-//     const div = document.createElement('div')
-//     for(const res of results){
-//         const name = results.title
-//         console.log(name)
-//     }
-//     // results.forEach(result => {
-//     //     const name = results.title
-//     //     console.log(name)
-//     // })
-// }
+        parentDiv.append(div)
+    })
+    // Adding style in
+    parentDiv.classList.add('text-center')
+    parentDiv.style.display = 'grid'
+    parentDiv.style.gridTemplateColumns = 'repeat(5, 1fr)'
+    // Adding All Element in Result-Box div
+    resultBox.append(parentDiv)
+}
